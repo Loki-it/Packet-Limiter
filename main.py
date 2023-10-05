eefb942827fb21c755dfca885097851c54f48dc0
Loki <60159705+Loki-it@users.noreply.github.com>
2023-10-05 17:22:40 +0200

    Update main.py
    Ora è possibile impostare il limite sia per i pacchetti Tcp che Udp

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import subprocess
 import os
 import re
 
-LIMIT = 1000
+TCP_LIMIT = 1000  # limite per pacchetti TCP
+UDP_LIMIT = 10000  # limite per pacchetti UDP
 INTERFACE = 'eth0'
 
-ip_counts = {}
+tcp_ip_counts = {}
+udp_ip_counts = {}
 blocked_ips = set()
 
 ssh_client_ip = os.environ.get('SSH_CLIENT', '').split(' ')[0] if 'SSH_CLIENT' in os.environ else None
@@ -16,16 +18,25 @@ def extract_attacker_ip(row):
     return ips[0] if ips else None
 
 try:
-    with subprocess.Popen(['tcpdump', '-n', '-l', '-i', INTERFACE, 'ip'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True) as process:
+    # Modificato per catturare sia pacchetti TCP che UDP
+    with subprocess.Popen(['tcpdump', '-n', '-l', '-i', INTERFACE, 'ip', 'and', '(tcp or udp)'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True) as process:
         for row in iter(process.stdout.readline, ''):
             attacker_ip = extract_attacker_ip(row)
             
             if not attacker_ip or attacker_ip == ssh_client_ip:
                 continue
             
-            ip_counts[attacker_ip] = ip_counts.get(attacker_ip, 0) + 1
+            # Determina se il pacchetto è TCP o UDP e aggiorna il conteggio corrispondente
+            if 'TCP' in row:
+                tcp_ip_counts[attacker_ip] = tcp_ip_counts.get(attacker_ip, 0) + 1
+                limit_reached = tcp_ip_counts[attacker_ip] > TCP_LIMIT
+            elif 'UDP' in row:
+                udp_ip_counts[attacker_ip] = udp_ip_counts.get(attacker_ip, 0) + 1
+                limit_reached = udp_ip_counts[attacker_ip] > UDP_LIMIT
+            else:
+                continue  # ignora altri tipi di pacchetti
             
-            if ip_counts[attacker_ip] > LIMIT and attacker_ip not in blocked_ips:
+            if limit_reached and attacker_ip not in blocked_ips:
                 subprocess.run(['iptables', '-A', 'INPUT', '-s', attacker_ip, '-j', 'DROP'])
                 blocked_ips.add(attacker_ip)
                 print(f"IP {attacker_ip} bloccato.")
